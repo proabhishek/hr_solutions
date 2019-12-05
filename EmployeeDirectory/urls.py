d5c2edda3a1d30ced401893c639037167f27ad66
Abhishek <theabhishek.srm@gmail.com>
2019-12-05 15:16:40 +0530

    updated employee urls

--- EmployeeDirectory/urls.py
+++ EmployeeDirectory/urls.py
@@ -4,9 +4,9 @@ from .views import *
 urlpatterns = [
     path('bulk-update/', BulkEmployeeUpdateExcel.as_view(),  name='bulk-update'),
     path('show-data/', ShowExcelData.as_view(),  name='show-data'),
-    # path('sign-in/', SignInView.as_view(),  name='employee-signin'),
-    # path('reset-password/', ResetPassword.as_view(),  name='employee-reset-password'),
-    # path('change-password/', ChangePassword.as_view(),  name='employee-change-password'),
-    # path('forgot-password/', SignInView.as_view(),  name='employee-signin'),
-    # path('logout/', Logout.as_view(),  name='logout'),
+    path('sign-in/', SignInView.as_view(),  name='employee-signin'),
+    path('reset-password/', ResetPassword.as_view(),  name='employee-reset-password'),
+    path('change-password/', ChangePassword.as_view(),  name='employee-change-password'),
+    path('forgot-password/', SignInView.as_view(),  name='employee-signin'),
+    path('logout/', Logout.as_view(),  name='logout'),
     ]
